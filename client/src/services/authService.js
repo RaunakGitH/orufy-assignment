0e8983cc221aeb605ea477f05a7a0c13809abe7c
Raunak Gargay <raunakgg@gmail.com>
2026-05-15 17:20:44 +0530

    Minor fixes

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,9 +1,9 @@
 import API from "./api";
 
 export const login = (emailOrPhone)=>{
-    API.post('/auth/login',{emailOrPhone})
+   return API.post('/auth/login',{emailOrPhone})
 }
 export const verifyOtp = (emailOrPhone,otp) => {
-    API.post('/auth/verify-otp',{emailOrPhone, otp})
+  return  API.post('/auth/verify-otp',{emailOrPhone, otp})
 
 }
